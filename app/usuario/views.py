b466b02e919772b8cf009bac3c81a8af820ae25f
yurihs <yurisalvador@hotmail.com>
2019-06-03 16:06:56 -0300

    Mover dados de exemplo do template para a view

--- app/usuario/views.py
+++ app/usuario/views.py
@@ -2,10 +2,20 @@ from flask import Blueprint, render_template
 
 usuario = Blueprint('usuario', __name__)
 
+usuarios = [
+    dict(id=1, nome='Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr.', email="hubertbw@example.com", cpf='109.985.380-00', tipo='Administrador'),
+    dict(id=2, nome='Fulano da Silva', email="fulanos@example.com", cpf='796.520.230-19', tipo='Funcionário'),
+    dict(id=3, nome='Alice', email="alice@example.com", cpf='832.557.570-02', tipo='Funcionário'),
+    dict(id=4, nome='Bob', email="bob@example.com", cpf='902.795.460-76', tipo='Funcionário'),
+    dict(id=5, nome='Charlie', email="charlie@example.com", cpf='882.450.240-74', tipo='Funcionário'),
+    dict(id=6, nome='Dave', email="dave@example.com", cpf='540.911.310-18', tipo='Funcionário'),
+]
+
 
 @usuario.route('/')
 def listar_usuarios():
-    return render_template('usuarios/lista.html')
+    return render_template('usuarios/lista.html', usuarios=usuarios)
+
 
 @usuario.route('/<int:id_usuario>')
 def editar(id_usuario):
