21e4c98b46211fc0f932a9b3cd12f445afdccc70
yurihs <yurisalvador@hotmail.com>
2019-06-04 10:09:25 -0300

    Adicionar base para o relatório geral

--- app/relatorio/views.py
+++ app/relatorio/views.py
@@ -1,9 +1,28 @@
-from flask import Blueprint, render_template
+from datetime import datetime
+from flask import Blueprint, render_template, request
 
 from app.empresa.views import empresas
+from app.porto.views import portos
 
 relatorio = Blueprint('relatorio', __name__)
+
 
 @relatorio.route('/')
 def index():
     return render_template('relatorio/index.html', empresas=empresas)
+
+
+@relatorio.route('/gerar/geral', methods=['POST'])
+def gerar_relatorio_geral():
+    try:
+        data_inicio = datetime.strptime(request.form.get('data_inicio'), '%Y-%m-%d').date()
+        data_termino = datetime.strptime(request.form.get('data_termino'), '%Y-%m-%d').date()
+    except (ValueError, TypeError) as e:
+        return str(e), 400
+
+    return render_template(
+        'relatorio/geral.html',
+        porto=portos[0],
+        data_inicio=data_inicio,
+        data_termino=data_termino
+    )
