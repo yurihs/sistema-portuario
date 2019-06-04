b80c90634a845fb76ab0e30ca72ae29ba95c8fe4
yurihs <yurisalvador@hotmail.com>
2019-06-03 22:36:37 -0300

    Arrumar chamada ao cleave.js

--- app/assets/scripts/usuario.js
+++ app/assets/scripts/usuario.js
@@ -1,5 +1,7 @@
-var cleaveCPF = new Cleave('input[name=cpf]', {
-    delimiters: ['.', '.', '-'],
-    blocks: [3, 3, 3, 2],
-    uppercase: true
+$(function () {
+    var cleaveCPF = new Cleave('input[name=cpf]', {
+        delimiters: ['.', '.', '-'],
+        blocks: [3, 3, 3, 2],
+        uppercase: true
+    });
 });
