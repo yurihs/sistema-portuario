f047a8affe81d39c5de8bbd100d5254edcd8a34f
yurihs <yurisalvador@hotmail.com>
2019-11-12 09:36:51 -0300

    Desabilitar autenticacao

--- backend/sistema_portuario/core/views.py
+++ backend/sistema_portuario/core/views.py
@@ -30,26 +30,28 @@ class ModelViewSet(
 class GrupoViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Group.objects.all()
     serializer_class = serializers.GrupoSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [permissions.DjangoModelPermissions]
 
 
 class UsuarioViewSet(ModelViewSet):
     queryset = models.Usuario.objects.all()
     serializer_class = serializers.UsuarioSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [permissions.DjangoModelPermissions]
 
     def retrieve(self, request, *args, **kwargs):
-        if not request.user.has_perm("core.view_usuario"):
-            raise PermissionDenied()
+        # if not request.user.has_perm("core.view_usuario"):
+        #     raise PermissionDenied()
         return super().retrieve(request, *args, **kwargs)
 
     def list(self, request, *args, **kwargs):
-        if not request.user.has_perm("core.view_usuario"):
-            raise PermissionDenied()
+        # if not request.user.has_perm("core.view_usuario"):
+        #     raise PermissionDenied()
         return super().list(request, *args, **kwargs)
 
     @action(detail=False, methods=["get"], url_path="atual")
     def get_current(self, request):
+        if request.user.is_anonymous:
+            raise PermissionDenied()
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
@@ -57,7 +59,7 @@ class UsuarioViewSet(ModelViewSet):
 class EmpresaViewSet(ModelViewSet):
     queryset = models.Empresa.objects.select_related("endereco")
     serializer_class = serializers.EmpresaSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [permissions.DjangoModelPermissions]
 
     @action(detail=True, methods=["get"], url_path="navios")
     @swagger_auto_schema(
@@ -74,7 +76,7 @@ class EmpresaViewSet(ModelViewSet):
 class TipoCargaViewSet(ModelViewSet):
     queryset = models.TipoCarga.objects.all()
     serializer_class = serializers.TipoCargaSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [permissions.DjangoModelPermissions]
 
 
 class NavioViewSet(ModelViewSet):
@@ -83,7 +85,7 @@ class NavioViewSet(ModelViewSet):
     ).prefetch_related("tipos_de_carga_suportados")
     serializer_class = serializers.NavioSerializer
     lookup_field = "numero_imo"
-    permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [permissions.DjangoModelPermissions]
 
     @action(detail=True, methods=["get"], url_path="viagens")
     @swagger_auto_schema(
@@ -101,7 +103,7 @@ class PortoViewSet(ModelViewSet):
     queryset = models.Porto.objects.select_related("endereco")
     serializer_class = serializers.PortoSerializer
     lookup_field = "un_locode"
-    permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [permissions.DjangoModelPermissions]
 
     @action(detail=True, methods=["get"], url_path="viagens")
     @swagger_auto_schema(
@@ -120,4 +122,4 @@ class ViagemViewSet(ModelViewSet):
         "navio", "navio__empresa__endereco", "porto_origem__endereco"
     ).prefetch_related("navio__tipos_de_carga_suportados", "cargas")
     serializer_class = serializers.ViagemSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [permissions.DjangoModelPermissions]
