f1b2a3450b70aa9644aab695542eed97425fe292
MarcosSchlickmann <marcos@boxti.com.br>
2019-11-15 19:51:22 -0300

    Criar rota principal e de alteração de usuário

--- frontend/src/_helpers/router.js
+++ frontend/src/_helpers/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// import Index from '@/components/Index'
+import Index from '@/components/Index'
+import AlterarUsuario from '@/components/usuarios/AlterarUsuario'
 
 Vue.use(Router)
 
@@ -12,7 +13,12 @@ export default new Router({
     {
       path: '/',
       name: 'Index',
-      // component: Index
+      component: Index
     },
-	]
+    {
+      path: '/usuarios/:id/alterar',
+      name: 'AlterarUsuario',
+      component: AlterarUsuario
+    },
+  ]
 })
---
+++ frontend/src/components/usuarios/AlterarUsuario.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ...">
+    Test
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      source: String,
+    },
+
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
