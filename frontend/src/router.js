a80e8ef253fa9d7fed9f74c47641f982de4ab650
MarcosSchlickmann <marcosamichels@gmail.com>
2019-11-24 19:18:12 -0300

    Ajustar rotas

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -28,44 +28,55 @@ export default new Router({
       name: 'Inicio',
       component: Inicio
     },
+
     {
       path: '/usuarios',
       name: 'ListagemUsuarios',
       component: ListagemUsuarios
     },
+    
     {
       path: '/usuarios/:id/alterar',
       name: 'AlterarUsuario',
       component: AlterarUsuario
     },
+    
     {
       path: '/navios/:numero_imo/alterar',
       name: 'AlterarNavio',
-      component: AlterarNavio
+      component: AlterarNavio,
+    },
+    
+    {
       path: '/empresas/:id/alterar',
       name: 'AlterarEmpresa',
       component: AlterarEmpresa
     },
+    
     {
       path: '/usuarios/cadastrar',
       name: 'CadastrarUsuario',
       component: CadastrarUsuario
     },
+    
     {
       path: '/tipos-carga/cadastrar',
       name: 'CadastrarTiposCargas',
       component: CadastrarTiposCarga
     },
+    
     {
       path: '/tipos-carga/:id/alterar',
       name: 'AlterarTiposCarga',
       component: AlterarTiposCarga
     },
+    
     {
       path: '/portos/:un_locode/alterar',
       name: 'AlterarPorto',
       component: AlterarPorto
     },
+    
     {
       path: '*',
       redirect: '/'
