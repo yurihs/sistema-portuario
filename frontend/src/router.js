07aa1bc969695805e08e9ce4725b6766c0ef25b4
Josder da Silveira Martins <32330547+JosderMartins@users.noreply.github.com>
2019-11-28 10:34:55 -0300

    Cadastrarporto (#79)
    * Cadastro de Tipos de Carga
    
    * ALterar e Exlcuir Tipos de Carga
    
    * Alterar porto, e  alterarempresa
    
    * cadastrar Porto
    
    * Alterar ordem das rotas para uma não substituir a outra

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -7,14 +7,18 @@ import CadastrarUsuario from '@/views/CadastrarUsuario'
 import AlterarUsuario from '@/views/AlterarUsuario'
 import ListagemUsuarios from '@/views/ListagemUsuarios'
 import ExibirUsuario from '@/views/ExibirUsuario'
+
 import AlterarEmpresa from '@/views/AlterarEmpresa'
-
-import AlterarPorto from '@/views/AlterarPorto'
-
-import AlterarNavio from '@/views/AlterarNavio'
 
 import CadastrarTiposCarga from '@/views/CadastrarTiposCarga'
 import AlterarTiposCarga from '@/views/AlterarTiposCarga'
+
+import AlterarPorto from '@/views/AlterarPorto'
+import CadastrarPorto from '@/views/CadastrarPorto'
+
+
+import AlterarNavio from '@/views/AlterarNavio'
+
 
 
 Vue.use(Router)
@@ -84,6 +88,26 @@ export default new Router({
       component: AlterarPorto
     },
     
+    {
+      path: '/tipos-carga/cadastrar',
+      name: 'CadastrarTiposCargas',
+      component: CadastrarTiposCarga
+    },
+    {
+      path: '/tipos-carga/:id/alterar',
+      name: 'AlterarTiposCarga',
+      component: AlterarTiposCarga
+    },
+    {
+      path: '/portos/cadastrar',
+      name: 'CadastrarPorto',
+      component: CadastrarPorto
+    },
+    {
+      path: '/portos/:un_locode/alterar',
+      name: 'AlterarPorto',
+      component: AlterarPorto
+    },
     {
       path: '*',
       redirect: '/'
